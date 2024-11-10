f8c0f90cfc6959866d4353ab5fd4274a48512cf5
Miguel-033 <programador.it@icloud.com>
2024-11-10 19:53:16 +0100

    commit

---
+++ public/assets/js/formSRM.js
@@ -0,0 +1,8 @@
+
+  id="bloknotappWidgetElement"
+  data-widget-lang="Онлайн запис"
+  data-widget-url="1ff10c93"
+  data-widget-color="#1280fe"
+  data-widget-position="left"
+  src="https://app.bloknotapp.com/widget/widget.js?nonce=0.11304688236054217"
+
